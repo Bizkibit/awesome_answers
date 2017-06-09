58d4ebb8a0e5c75c971b5d2d3511572230d53439
Farshid HM <fhmanesh@gmail.com>
2017-06-09 14:44:41 -0700

    created questions voter helper method

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,20 @@
 module VotesHelper
+
+  def question_voter(question, vote)
+    # if vote.nil?
+     [
+      link_to(
+        fa_icon('chevron-up lg'),
+        question_votes_path(question, { is_up: true }),
+        method: :post
+      ),
+      question.vote_total,
+      link_to(
+        fa_icon('chevron-down lg'),
+        question_votes_path(question, { is_up: false }),
+        method: :post
+      )
+    ].join('')
+  # end
+  end
 end
