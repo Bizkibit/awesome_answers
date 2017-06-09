fdae3505ecade08857b45e1e53ade749c35d24d3
Farshid HM <fhmanesh@gmail.com>
2017-06-09 13:26:43 -0700

    added arrows to show

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,5 @@ class Vote < ApplicationRecord
   belongs_to :question
 
   validates :question_id, uniqueness: {scope: :user_id, message: 'has already been voted on by yourself'}
+  validates :is_up, presence: true
 end
