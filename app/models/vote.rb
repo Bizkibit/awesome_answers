814cc271079a80d7d3d09ab6256fe1010527360c
Farshid HM <fhmanesh@gmail.com>
2017-06-09 13:17:23 -0700

    added uniquness in votes model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :question
+
+  validates :question_id, uniqueness: {scope: :user_id, message: 'has already been voted on by yourself'}
 end
