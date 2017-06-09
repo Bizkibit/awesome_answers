d7e92a8dca1cde958ca04050481108313b9048c7
Farshid HM <fhmanesh@gmail.com>
2017-06-09 14:17:48 -0700

    added note adn before action

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,5 @@
 class VotesController < ApplicationController
+  before_action :find_question, only: [:create]
 
   def create
     vote = Vote.new (user: current_user, question: @question, is_up: params[:is_up])
@@ -7,6 +8,10 @@ class VotesController < ApplicationController
     else
       flash[:alert] = vote.pretty_errors
     end
+    #redirect_to question_path(@question)
+    #as a shortcut, rails allows to redirect to an
+    #instance of model instead of redirect_to question_path(@question)
+    #this will only work if your routes follow rails convention
     redirect_to @question
   end
 
