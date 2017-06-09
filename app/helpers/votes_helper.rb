ec6ba3e8b3d93d18fadbbff67611c6337ae01cac
Farshid HM <fhmanesh@gmail.com>
2017-06-09 14:59:42 -0700

    added downvote remove functionality

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -10,18 +10,18 @@ module VotesHelper
         method: :post
     elsif vote.is_up?
       upvote = link_to fa_icon('chevron-up 2x'),
-        question_vote_path(@question, @vote),
+        question_vote_path(question, vote),
         method: :delete
       downvote = link_to fa_icon('chevron-down lg'),
-        question_votes_path(question, { is_up: false }),
-        method: :post
-    else
-      upvote = link_to fa_icon('chevron-up 2x'),
-        question_vote_path(@question, @vote, { is_up: true }),
+        question_vote_path(question, vote, { is_up: false }),
         method: :patch
-      downvote = link_to fa_icon('chevron-down lg'),
-        question_votes_path(question, { is_up: false }),
-        method: :post
+    else
+      upvote = link_to fa_icon('chevron-up lg'),
+        question_vote_path(question, vote, { is_up: true }),
+        method: :patch
+      downvote = link_to fa_icon('chevron-down 2x'),
+        question_vote_path(question, vote),
+        method: :delete
     end
 
     [ upvote, question.vote_total, downvote ].join('').html_safe
