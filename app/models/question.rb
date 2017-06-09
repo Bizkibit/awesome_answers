0ca12fc39867bf5167eead216ae620b8830b9495
Farshid HM <fhmanesh@gmail.com>
2017-06-09 15:18:51 -0700

    added relationships

--- app/models/question.rb
+++ app/models/question.rb
@@ -45,6 +45,10 @@ class Question < ApplicationRecord
   has_many :votes, dependent: :destroy
   has_many :voters, through: :votes, source: :user
 
+  has_many :taggings, dependent: :destroy
+  #we dont have to give has_many a source when the name of the relationship is the same as the source
+  has_many :tags, through: :taggings
+
   validates(:title, { presence: { message: 'must be provided' },
                       uniqueness: true })
   validates(:body, { length: { minimum: 5, maximum: 1000 }})
