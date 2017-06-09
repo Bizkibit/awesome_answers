b43debbed5874980af7e94b29a347445a7f24c3c
Farshid HM <fhmanesh@gmail.com>
2017-06-09 14:14:46 -0700

    set create method for vote controller and define private method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,13 @@
 class VotesController < ApplicationController
 
   def create
-
+    vote = Vote.new (user: current_user, question: @question, is_up: params[:is_up])
+    if vote.save
+      flash[:notice] = 'Thanks for voting!'
+    else
+      flash[:alert] = vote.pretty_errors
+    end
+    redirect_to @question
   end
 
   def update
@@ -11,4 +17,15 @@ class VotesController < ApplicationController
   def destroy
 
   end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def find_vote
+    @vote = Vote.find(params[:id])
+  end
+
 end
